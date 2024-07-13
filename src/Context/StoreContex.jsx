1a848cf63ff2053d4050ac3e5bb8de6c1c321d6f
JANAK <98094845+janaksanjel@users.noreply.github.com>
2024-07-13 19:05:58 +0545

    Update StoreContex.jsx

--- src/Context/StoreContex.jsx
+++ src/Context/StoreContex.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { createContext, useEffect, useState } from "react";
+import { food_list } from "../assets/assets";
 
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
